e8065cafe4208ff1d21e9c9b42fc8ce78cf38a00
Patrick Rills <patrickrills@patricks-macbook-pro.local>
2016-03-19 14:50:07 -0400

    Add video player file

---
+++ CustomMLBHighlights/VideoPlayer.h
@@ -0,0 +1,13 @@
+//
+//  VideoPlayer.h
+//  CustomMLBHighlights
+//
+//  Created by Patrick Rills on 3/19/16.
+//  Copyright © 2016 BaseballHackDay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoPlayer : UIViewController
+
+@end
---
+++ CustomMLBHighlights/VideoPlayer.m
@@ -0,0 +1,13 @@
+//
+//  VideoPlayer.m
+//  CustomMLBHighlights
+//
+//  Created by Patrick Rills on 3/19/16.
+//  Copyright © 2016 BaseballHackDay. All rights reserved.
+//
+
+#import "VideoPlayer.h"
+
+@implementation VideoPlayer
+
+@end
