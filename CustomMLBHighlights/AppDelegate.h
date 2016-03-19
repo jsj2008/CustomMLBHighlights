6195214d59918618d58c58f1108b6954a4eccb80
Patrick Rills <patrickrills@patricks-macbook-pro.local>
2016-03-19 11:03:06 -0400

    Initial Commit

---
+++ CustomMLBHighlights/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomMLBHighlights
+//
+//  Created by Patrick Rills on 3/19/16.
+//  Copyright © 2016 BaseballHackDay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomMLBHighlights/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomMLBHighlights
+//
+//  Created by Patrick Rills on 3/19/16.
+//  Copyright © 2016 BaseballHackDay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
